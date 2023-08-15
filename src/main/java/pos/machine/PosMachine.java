baec05e82dfdf86db93b3a79bf74f024a6266ff3
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-15 21:09:30 +0800

    feat:get quantity of barcodes

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -9,11 +9,26 @@ public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         Map<String, Long> groupBarcodes = groupAllBarcodes(barcodes);
 
+        for (String barcode : loadBarcodes()) {
+            groupBarcodes.getOrDefault(barcode, 0L);
+            findItemByBarcode(barcode);
+        }
+
         return groupBarcodes.toString();
     }
 
     private Map<String, Long> groupAllBarcodes(List<String> barcodes) {
         return barcodes.stream().collect(Collectors.groupingBy(barcode -> barcode, Collectors.counting()));
+    }
+    private List<String> loadBarcodes() {
+        return Arrays.asList("ITEM000000", "ITEM000001", "ITEM000004");
+    }
+
+    private Item findItemByBarcode(String barcode) {
+        return items.stream()
+                .filter(item -> item.getBarcode().equals(barcode))
+                .findFirst()
+                .orElse(null);
     }
 
 }
