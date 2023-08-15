c29a6d2cb7890846958b649bb8a91c3832b374a9
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-15 21:20:34 +0800

    Added append to display receipt values

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -9,12 +9,15 @@ public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         Map<String, Long> groupBarcodes = groupAllBarcodes(barcodes);
         int total = 0;
+
+        StringBuilder receiptBuilder = new StringBuilder("***<store earning no money>Receipt***\n");
         for (String barcode : loadBarcodes()) {
           long quantity = groupBarcodes.getOrDefault(barcode, 0L);
           Item item = findItemByBarcode(barcode);
 
             if (item != null && quantity > 0) {
                 int subtotal = calculateSubtotal(item.getPrice(), quantity);
+                appendReceiptLine(receiptBuilder, item.getName(), quantity, item.getPrice(), subtotal);
                 total += subtotal;
             }
         }
@@ -41,4 +44,8 @@ public class PosMachine {
         return price * (int) quantity;
     }
 
+    private void appendReceiptLine(StringBuilder receiptBuilder, String name, long quantity, int price, int subtotal) {
+        receiptBuilder.append(String.format("Name: %s, Quantity: %d, Unit price: %d (yuan), Subtotal: %d (yuan)\n",
+                name, quantity, price, subtotal));
+    }
 }
