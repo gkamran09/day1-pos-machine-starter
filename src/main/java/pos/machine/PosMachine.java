a87895ed37bdae9d46ed2fa95bf32ada7d401342
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-15 21:00:05 +0800

    feat:grouped all barcodes in the map

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,19 @@
 package pos.machine;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PosMachine {
+    private static final List<Item> items = ItemsLoader.loadAllItems();
+
     public String printReceipt(List<String> barcodes) {
-        return null;
+        Map<String, Long> groupBarcodes = groupAllBarcodes(barcodes);
+
+        return groupBarcodes.toString();
     }
+
+    private Map<String, Long> groupAllBarcodes(List<String> barcodes) {
+        return barcodes.stream().collect(Collectors.groupingBy(barcode -> barcode, Collectors.counting()));
+    }
+
 }
