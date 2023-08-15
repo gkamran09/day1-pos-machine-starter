c608ebb3eff90d7fa72182ff90271818a843180d
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-15 21:16:12 +0800

    feat:added computation for subtotal and total amount

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -8,11 +8,17 @@ public class PosMachine {
 
     public String printReceipt(List<String> barcodes) {
         Map<String, Long> groupBarcodes = groupAllBarcodes(barcodes);
-
+        int total = 0;
         for (String barcode : loadBarcodes()) {
-            groupBarcodes.getOrDefault(barcode, 0L);
-            findItemByBarcode(barcode);
+          long quantity = groupBarcodes.getOrDefault(barcode, 0L);
+          Item item = findItemByBarcode(barcode);
+
+            if (item != null && quantity > 0) {
+                int subtotal = calculateSubtotal(item.getPrice(), quantity);
+                total += subtotal;
+            }
         }
+
 
         return groupBarcodes.toString();
     }
@@ -29,6 +35,10 @@ public class PosMachine {
                 .filter(item -> item.getBarcode().equals(barcode))
                 .findFirst()
                 .orElse(null);
+    }
+
+    private int calculateSubtotal(int price, long quantity) {
+        return price * (int) quantity;
     }
 
 }
